f6ac7ba2f30cdaafb420d8fb696856c5fa0cbc96
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-01-08 14:38:35 +0900

    init: zolaへの移行のための大幅削除

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-const SiteSetting = {
-  SocialLinks: {
-    Discord: "https://discord.com/invite/Nus2xg3nF3",
-    GitHub: "https://github.com/Rust-Developers-JP",
-  },
-};
-
-export default SiteSetting;
